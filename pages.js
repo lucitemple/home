ff392205c212da0eea66c930275aa715afa52710
Luci Temple <lucitemple@yahoo.com.au>
2021-01-25 12:51:12 +1100

    Reconfigure repoURL

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://lucitemple.github.io/home/";
+const repoURL = "https://github.com/lucitemple/lucitemple.git.io.git";
 
 ghpages.publish(
   pathname,
